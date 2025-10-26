c916885b94e8a8c92d91031a0ae0f503f81cfd0a
Leinad745 <da.corderom@duocuc.cl>
2025-10-26 16:24:32 -0300

    Sincronizacion main

--- h4cker-f1nd3r/src/equipo.jsx
+++ h4cker-f1nd3r/src/equipo.jsx
@@ -1,8 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-//import { useNavigate } from "react-router-dom";
 
 export default function GestorEquipo() {
-    {/*const navigate = useNavigate();*/}
+    
     return (
         <h1>Gestor de Equipo</h1>
     )
