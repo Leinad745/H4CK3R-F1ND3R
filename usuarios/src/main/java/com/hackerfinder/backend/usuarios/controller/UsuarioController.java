55e620a2dd588d0d9752ff0155eabb34071180c0
Leinad745 <da.corderom@duocuc.cl>
2025-12-02 18:32:30 -0300

    Conexion con api en registro

--- usuarios/src/main/java/com/hackerfinder/backend/usuarios/controller/UsuarioController.java
+++ usuarios/src/main/java/com/hackerfinder/backend/usuarios/controller/UsuarioController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import com.hackerfinder.backend.usuarios.service.UsuarioService;
 import com.hackerfinder.backend.usuarios.model.Usuario;
 import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
