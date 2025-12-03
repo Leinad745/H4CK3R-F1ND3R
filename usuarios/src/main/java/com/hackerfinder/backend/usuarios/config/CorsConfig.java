358a60a447d8bd9cdec56a54dd247583340b63e4
Leinad745 <da.corderom@duocuc.cl>
2025-12-02 21:26:53 -0300

    Resolucion de conflictos

--- usuarios/src/main/java/com/hackerfinder/backend/usuarios/config/CorsConfig.java
+++ usuarios/src/main/java/com/hackerfinder/backend/usuarios/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
                             "http://localhost:3000",
                             "http://127.0.0.1:5173"
                         )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
